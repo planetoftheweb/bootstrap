2054ee2b3330ed5ced5cf00bd8720a1afd9b2266
Ray Villalobos <ray@planetoftheweb.com>
2015-07-07 16:59:32 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    bootlint = require('gulp-bootlint'),
+    bob = require('bob')
+    webserver = require('gulp-webserver');
+
+gulp.task('js', function() {
+  return gulp.src('builds/bootstrap/js/myscript.js')
+    .pipe(jshint('./.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('html', function() {
+  gulp.src('builds/bootstrap/**/*.html')
+  .pipe(bootlint());
+});
+
+gulp.task('css', function() {
+    gulp.src('builds/bootstrap/css/*.css');
+});
+
+gulp.task('watch', function() {
+    gulp.watch('builds/bootstrap/js/**/*', ['js']);
+    gulp.watch('builds/bootstrap/css/**/*.css', ['css']);
+    gulp.watch(['builds/bootstrap/**/*.html'
+    ], ['html']);
+});
+
+gulp.task('webserver', function() {
+    gulp.src('builds/bootstrap/')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
